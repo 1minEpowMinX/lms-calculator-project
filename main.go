e5ec278439b531dd17c781295706f0e8a7795812
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-02-23 20:15:15 +0000

    Update packages name

--- main.go
+++ main.go
@@ -2,36 +2,36 @@ package main
 
 import (
 	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 func main() {
+	// Получение текущей директории
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Ошибка при получении текущей директории: %v", err)
+	}
+
 	// Пути к файлам бекенда и фронта
-	backendPath := "http/backend/back-server.go"
-	frontendPath := "http/frontend/front-server.go"
+	backendPath := filepath.Join(currentDir, "http", "backend", "back-server.go")
+	frontendPath := filepath.Join(currentDir, "http", "frontend", "front-server.go")
 
 	// Запуск бекенда
-	go runBackend(backendPath)
+	go runServer(backendPath)
 
 	// Запуск фронта
-	go runFrontend(frontendPath)
+	go runServer(frontendPath)
 
 	// Бесконечный цикл, чтобы программа не завершилась сразу после запуска
 	select {}
 }
 
-func runBackend(backendPath string) {
-	cmd := exec.Command("go", "run", backendPath)
+func runServer(filePath string) {
+	cmd := exec.Command("go", "run", filePath)
 	err := cmd.Run()
 	if err != nil {
-		log.Fatalf("Ошибка при запуске бекенда: %v", err)
-	}
-}
-
-func runFrontend(frontendPath string) {
-	cmd := exec.Command("go", "run", frontendPath)
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("Ошибка при запуске фронта: %v", err)
+		log.Fatalf("Ошибка при запуске сервера: %v", err)
 	}
 }
