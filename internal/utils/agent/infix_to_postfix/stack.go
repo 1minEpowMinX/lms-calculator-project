51e4cbb4a51d6145cb3c14f31effbd4657aba384
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-04-22 09:10:25 +0000

    rewrite stack struct with slices

--- internal/utils/agent/infix_to_postfix/stack.go
+++ internal/utils/agent/infix_to_postfix/stack.go
@@ -1,41 +1,29 @@
 package infix_to_postfix
 
 type Stack struct {
-	Top  *Element
-	Size int
+	Items []interface{}
 }
 
-// Element структура для каждого элемента в стеке
-// value понятно - само значение
-// next - замена верхнего элемента последующим (нужна только для Pop())
-type Element struct {
-	Value interface{}
-	Next  *Element
-}
-
-// Empty есть там че нить или нет, проверяем
 func (s *Stack) Empty() bool {
-	return s.Size == 0
+	return len(s.Items) == 0
 }
 
-// Top самый верхний элемент стека
 func (s *Stack) TopFunc() interface{} {
-	return s.Top.Value
+	if !s.Empty() {
+		return s.Items[len(s.Items)-1]
+	}
+	return nil
 }
 
-// Push заносим элемент в стек
 func (s *Stack) Push(value interface{}) {
-	s.Top = &Element{value, s.Top}
-	s.Size++
+	s.Items = append(s.Items, value)
 }
 
-// Pop выносим элемент из стека
-// да знаю можно было срезАть слайс, но я без слайса реализовал и из-за этого костыли :)
 func (s *Stack) Pop() (value interface{}) {
-	if s.Size > 0 {
-		value, s.Top = s.Top.Value, s.Top.Next
-		s.Size--
-		return
+	if !s.Empty() {
+		top := s.Items[len(s.Items)-1]
+		s.Items = s.Items[:len(s.Items)-1]
+		return top
 	}
 	return nil
 }
